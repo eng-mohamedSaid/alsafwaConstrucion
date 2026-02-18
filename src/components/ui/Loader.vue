870572d328078c5231bf42f52a0bfe2e23b1ce3f
m2001said <ms277712@gmail.com>
2026-02-18 12:24:34 +0200

    edit loader

--- src/components/ui/Loader.vue
+++ src/components/ui/Loader.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="loader-container">
     <div class="loader">
-      <img class="logo-icon" src="../../assets/images/elsheikh-groups-icon.svg" />
+      <img
+        class="logo-icon"
+        src="../../assets/images/elsheikh-groups-icon.svg"
+      />
 
       <p class="alsafwa">AL El Sheikh</p>
-      <p class="construction">Group</p>
+      <p class="alsafwa">Group</p>
     </div>
   </div>
 </template>
@@ -23,22 +26,19 @@
   height: 100vh;
 }
 .logo-icon {
-  max-width: 150px;
+  max-width: 120px;
   animation: heartbeat 1.5s ease-in-out infinite both;
+  margin-bottom: 15px;
 }
 
 .alsafwa {
   font-size: 2.3rem;
   font-weight: bold;
+  text-align: "center";
+  line-height: 35px;
   color: var(--color-dark);
-  animation: slide-in-elliptic-top-fwd 0.7s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
-}
-.construction {
-  font-size: 2.3rem;
-  margin-top: -28px;
-  font-weight: bold;
-  color: var(--color-dark);
-  animation: slide-in-bottom 1.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+  animation: slide-in-elliptic-top-fwd 0.7s cubic-bezier(0.25, 0.46, 0.45, 0.94)
+    both;
 }
 
 @keyframes heartbeat {
@@ -64,16 +64,6 @@
     animation-timing-function: ease-out;
   }
 }
-@keyframes slide-in-bottom {
-  0% {
-    transform: translateY(1000px);
-    opacity: 0;
-  }
-  100% {
-    transform: translateY(0);
-    opacity: 1;
-  }
-}
 
 @keyframes slide-in-elliptic-top-fwd {
   0% {
@@ -85,6 +75,17 @@
     transform: translateY(0) rotateX(0) scale(1);
     transform-origin: 50% 1400px;
     opacity: 1;
+  }
+}
+
+@media (max-width: 600px) {
+  .logo-icon {
+    max-width: 90px;
+    margin-bottom: 10px;
+  }
+  .alsafwa {
+    font-size: 1.5rem;
+    line-height: 26px;
   }
 }
 </style>
